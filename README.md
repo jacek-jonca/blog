28f9db5d0427454ddb04096a89ef976dccdd58c6
Jacek Jonca-Jasinski <jacek@air.attlocal.net>
2020-02-21 20:37:13 -0600

    added travis config, readme, and deploy script

---
+++ README.md
@@ -0,0 +1,13 @@
+[![Build Status](https://img.shields.io/travis/jacek-jonca/blog?style=for-the-badge)](https://travis-ci.org/jacek-jonca/blog)
+
+NOTES:
+
+If using shell scripts chmod +x ./*.sh
+
+If hardcoding github access token see https://docs.travis-ci.com/user/environment-variables/#encrypting-environment-variables
+
+or
+
+set $GH_TOKEN at https://travis-ci.org/jacek-jonca/blog/settings
+
+COMPILED site: https://jonca.org
---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+git clone https://github.com/jacek-jonca/jacek-jonca.github.io.git
+cp -ap /public/. /jacek-jonca.github.io/
+
+if [ -n "$GH_TOKEN" ]
+then
+    touch ~/.git-credentials
+    chmod 0600 ~/.git-credentials
+    echo $GH_TOKEN > ~/.git-credentials
+    git config credential.helper store
+    git config --global user.email "${GIT_EMAIL}"
+    git config --global user.name "${GIT_NAME}"
+    git config --global push.default simple
+fi
+#git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+cs jacek-jonca.github.io
+git add --all
+git commit -m "Rebuild site"
+#git push --force origin HEAD:master
+git push
