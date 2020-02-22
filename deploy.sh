bcdf0d37a0c6328a7228fcba8ae2a3eef30c12d5
Jacek Jonca-Jasinski <jacek@air.attlocal.net>
2020-02-21 21:26:22 -0600

    revised authentication to GH

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 git clone https://github.com/jacek-jonca/jacek-jonca.github.io.git
 cp -ap /public/. /jacek-jonca.github.io/
-
+cd jacek-jonca.github.io
+git remote set-url origin https://jacek-jonca:${GH_TOKEN}@github.com/jacek-jonca/jacek-jonca.github.io.git
 #if [ -n "$GH_TOKEN" ]
 #then
 #    touch ~/.git-credentials
@@ -14,7 +15,7 @@ cp -ap /public/. /jacek-jonca.github.io/
 #    git config --global push.default simple
 #fi
 #git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
-cs jacek-jonca.github.io
+#cs jacek-jonca.github.io
 git add --all
 git commit -m "Rebuild site"
 #git push --force origin HEAD:master
